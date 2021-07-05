1a32b61f36015dcf83bd3c4cd134a148867feacc
jeffzi <jzinque@gmail.com>
2021-07-05 23:33:00 +0200

    fix layout + add del on layer

--- keymap.c
+++ keymap.c
@@ -43,12 +43,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,      KC_W,    KC_E,    KC_R,   KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,             KC_RBRC,             KC_BSLS,                      KC_PGUP,
         KC_LGUI, KC_A,      KC_S,    KC_D,    KC_F,   KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                                  KC_ENT,                       KC_PGDN,
         KC_LSPO, KC_Z,      KC_X,    KC_C,    KC_V,   KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                                           KC_RSPC,             KC_UP,   KC_END,
-        KC_LCTL, SNAKECASE, KC_LALT,                            KC_SPC,                             KC_RAL    MO(1),              KC_RCTL,             KC_LEFT,             KC_DOWN, KC_RGHT
+        KC_LCTL, SNAKECASE, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),              KC_RCTL,             KC_LEFT,             KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT(
         _______, KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,              KC_F12,              KC_SYSTEM_POWER,              _______,
-        _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______,             _______,                      _______,
+        _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______,             KC_DEL,                      _______,
         _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______,             RESET,                        _______,
         _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                                  _______,                      _______,
         _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                                  _______,             _______, _______,
